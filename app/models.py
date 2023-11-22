456ca8f8db1b657ee1e772118ae95176f0a4dab0
heusser-dev <heusser.escamilla@gmail.com>
2023-11-21 22:34:35 -0300

    bd 1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    descuento = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.nombre
+        
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=5, decimal_places=2)
+    stock = models.IntegerField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+
 # Create your models here.
--- app/views.py
+++ app/views.py
@@ -3,7 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'app/home.html')
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    return render(request,'app/home.html',)
 
 def contacto(request):
     return render(request, 'app/contacto.html')
