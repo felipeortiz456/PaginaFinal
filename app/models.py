88b7349eeca697d715140e4550e515fad9669e89
heusser-dev <heusser.escamilla@gmail.com>
2023-12-28 01:13:33 -0300

    cart

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Categoria(models.Model):
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=6, decimal_places=3)
+    precio = models.IntegerField()
     stock = models.IntegerField()
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
@@ -38,6 +38,34 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre
 
+class Carrito(models.Model):
+    id = models.AutoField(primary_key=True)
+    productos = models.ManyToManyField(Producto)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def calcular_total(self):
+        # Método para calcular el total del carrito basado en los productos actuales
+        self.total = sum(producto.precio for producto in self.productos.all())
+        self.save()
+
+    def agregar_producto(self, producto):
+        # Método para agregar un producto al carrito
+        self.productos.add(producto)
+        self.calcular_total()
+
+    def quitar_producto(self, producto):
+        # Método para quitar un producto del carrito
+        self.productos.remove(producto)
+        self.calcular_total()
+
+    def limpiar_carrito(self):
+        # Método para vaciar el carrito
+        self.productos.clear()
+        self.total = 0.00
+        self.save()
+
+    def __str__(self):
+        return f"Carrito {self.id}"
 
 class Pedido(models.Model):
     ESTADOS_PEDIDO = [
